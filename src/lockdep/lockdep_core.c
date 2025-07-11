b681b7fccef8b855ab6dcd62a1adc04994864c70
yuri <yuri@yuri.localdomain>
2025-07-11 20:47:21 -0300

    Removendo função 'remove_lock' e alterando mutex para função lockdep_acquire_lock().

--- src/lockdep/lockdep_core.c
+++ src/lockdep/lockdep_core.c
@@ -15,7 +15,7 @@ static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 static lock_node_t* lock_registry = NULL;
 
-void* smalloc(size_t bytes) {
+static void* smalloc(size_t bytes) {
     void* v = malloc(bytes);
     if (v == NULL) {
         fprintf(stderr, "out of memory\n");
@@ -31,39 +31,14 @@ static lock_node_t* find_or_create_lock(void* lock_addr) {
 
     if (ptr) return ptr;
 
-    pthread_mutex_lock(&mutex);
     lock_node_t* new = smalloc(sizeof *new);
 
     new->lock_addr = lock_addr;
     new->next = lock_registry;
 
-    pthread_mutex_unlock(&mutex);
     return lock_registry = new;
 }
 
-static void remove_lock(void* lock_addr) {
-    if (!lock_registry) return;
-
-    lock_node_t* node;
-
-    if (lock_registry->lock_addr == lock_addr) {
-        node = lock_registry;
-        lock_registry = node->next;
-        free(node);
-        return;
-    }
-
-    lock_node_t* ptr = lock_registry;
-    while (ptr->next && ptr->next->lock_addr != lock_addr) {
-        ptr = ptr->next;
-    }
-
-    if (!ptr->next) return;
-
-    node = ptr->next;
-    ptr->next = node->next;
-    free(node);
-}
 void lockdep_init(void) {
     const char* env = getenv("LOCKDEP_DISABLE");
     if (env && strcmp(env, "1") == 0) {
@@ -82,12 +57,13 @@ void lockdep_cleanup(void) {
 bool lockdep_acquire_lock(void* lock_addr) {
     printf("[LOCKDEP] Acquiring lock at %p\n", lock_addr);
 
+    pthread_mutex_lock(&mutex);
     find_or_create_lock(lock_addr);
+    pthread_mutex_unlock(&mutex);
     // verificar se tem ciclo
     return true;
 }
 
 void lockdep_release_lock(void* lock_addr) {
     printf("[LOCKDEP] Releasing lock at %p\n", lock_addr);
-    remove_lock(lock_addr);
 }
